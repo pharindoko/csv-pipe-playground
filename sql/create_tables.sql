d0fd604fe2079d42f067f750f907ccbf26f1cf8b
uid10804 <florian.fuss@vitesco.com>
2021-08-07 22:38:56 +0000

    feat(database): store csv content in postgre as json

---
+++ sql/create_tables.sql
@@ -0,0 +1,4 @@
+-- Creation of product table
+CREATE TABLE IF NOT EXISTS event (
+  eventdata JSON NOT NULL
+);
--- worker.py
+++ worker.py
@@ -1,7 +1,11 @@
-from celery import Celery
-import pandas as pd
-import time
 import gc
+import json
+import time
+
+import pandas as pd
+import psycopg2
+from celery import Celery
+
 app = Celery('tasks', broker='pyamqp://guest@localhost//')
 
 
@@ -11,7 +15,21 @@ def add(filepath: str):
     try:
         df = pd.read_csv(filepath)
         jsontest = df.to_json()
-        print(f'jsonstring: {jsontest}')
+        print(f'jsonstring: {jsontest[:100]}')
+
+        conn = psycopg2.connect(host='localhost',
+
+                                user='postgres',
+                                password='postgres',
+                                database='postgres')  # To remove slash
+        print(f'connect: {str(conn)}')
+        cursor = conn.cursor()
+        cursor.execute(
+            f"INSERT INTO public.event (eventdata) VALUES(%s)", (jsontest,))
+        conn.commit()
+        print(f'inserted record')
+        cursor.close()
+        conn.close()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
